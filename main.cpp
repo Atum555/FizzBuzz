b6e97cba93ae9cc0d5caf1dbbeb52fe79637af92
Claudio Meireles <claudiomeireles555@gmail.com>
2021-08-17 23:43:02 +0100

    Space optimization

--- main.cpp
+++ main.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
-
 void FizzBuzz(int Number) {
    int x = 0;
    if (Number % 3 == 0) {x = 1;}
    if (Number % 5 == 0) {x += 2;}
-
-   if (x == 0) {
-      std::cout << Number;
-      std::cout << "\n";
-   }
-
-   else if (x == 1) {
-      std::cout << "Fizz\n";
-   }
-   
-   else if (x == 2) {
-      std::cout << "Buzz\n";
-   }
-
-   else if (x == 3) {
-      std::cout << "FizzBuzz\n";
-   }
+   if (x == 0) {std::cout << Number; std::cout << "\n";}
+   else if (x == 1) {std::cout << "Fizz\n";}
+   else if (x == 2) {std::cout << "Buzz\n";}
+   else if (x == 3) {std::cout << "FizzBuzz\n";}
 }
-
 int main() {
    int i = 1;
    std::cout << "Number: ";
    int a;
    std::cin >> a;
-   while (i <= a) {
-      FizzBuzz(i);
-      i ++;
-   }
-
+   while (i <= a) {FizzBuzz(i); i ++;}
    return 0;
 }
