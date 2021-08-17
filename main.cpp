e5d00ebbbafbbe1f3fe3e7626c18a8598bc32f4e
Claudio Meireles <claudiomeireles555@gmail.com>
2021-08-17 23:48:27 +0100

    Final

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ void FizzBuzz(int Number) {
    int x = 0;
    if (Number % 3 == 0) {x = 1;}
    if (Number % 5 == 0) {x += 2;}
-   if (x == 0) {std::cout << Number;  std::cout << "\n";}
+   if (x == 0) {std::cout << Number; std::cout << "\n";}
    else if (x == 1) {std::cout << "Fizz\n";}
    else if (x == 2) {std::cout << "Buzz\n";}
    else if (x == 3) {std::cout << "FizzBuzz\n";}
